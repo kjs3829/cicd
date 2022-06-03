c7e95ef198864096f5eb426742dca819c3f5687e
JunSeong <kjs35323@naver.com>
2022-06-03 14:34:54 +0900

    a

---
+++ src/main/java/com/example/demo/ProfileController.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+
+@AllArgsConstructor
+@RestController
+public class ProfileController {
+    private final Environment environment;
+
+    @GetMapping("/profile")
+    public String profile() {
+        return Arrays.stream(environment.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+    }
+}
